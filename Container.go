e2d4f1a4da108721b44c01ac1de524dc8ff3bd6a
Sérgio Fonseca <sergio.fonseca@cappta.com.br>
2017-10-06 15:40:03 -0300

    MAE118 Added ArrayAppendOrCreate, ArrayAppendOrCreateP and LogAndSetError

--- Container.go
+++ Container.go
@@ -1,25 +1,34 @@
 package gohelpgabs
 
-import "github.com/Jeffail/gabs"
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/Jeffail/gabs"
+)
+
+var (
+	defaultErrorPath = "Errors"
+)
 
 // Container encapsulates gabs.Container
 type Container struct {
 	*gabs.Container
+	errorPath string
 }
 
 // New - Create a new gohelpgabs JSON object.
 func New() *Container {
-	return &Container{gabs.New()}
+	return &Container{gabs.New(), defaultErrorPath}
 }
 
 // ParseJSON parses a string into a representation of the parsed JSON in gabs and returns our container
 func ParseJSON(sample []byte) (container *Container, err error) {
-	gabsContainer, err := gabs.ParseJSON(sample)
-	if err != nil {
-		return
+	var gabsContainer *gabs.Container
+	if gabsContainer, err = gabs.ParseJSON(sample); err == nil {
+		container = &Container{gabsContainer, defaultErrorPath}
 	}
-
-	container = &Container{gabsContainer}
 	return
 }
 
@@ -47,7 +56,7 @@ func (container *Container) PopPath(path string) *Container {
 		return nil
 	}
 	container.DeleteP(path)
-	return &Container{popped}
+	return &Container{popped, container.errorPath}
 }
 
 // Search - Attempt to find and return an object within the JSON structure by specifying the
@@ -59,5 +68,30 @@ func (container *Container) Search(hierarchy ...string) *Container {
 	if gabsContainer == nil {
 		return nil
 	}
-	return &Container{gabsContainer}
+	return &Container{gabsContainer, container.errorPath}
+}
+
+// ArrayAppendOrCreate - Append a value onto a JSON array or create one with the provided value
+// if the array does not yet exists.
+func (container *Container) ArrayAppendOrCreate(value interface{}, path ...string) (err error) {
+	if err = container.ArrayAppend(value, path...); err == gabs.ErrNotArray {
+		if _, err = container.Array(path...); err != nil {
+			return
+		}
+		err = container.ArrayAppend(value, path...)
+	}
+	return
+}
+
+// ArrayAppendOrCreateP - Append a value onto a JSON array using a dot notation JSON path or
+// create one with the provided value if the array does not yet exists.
+func (container *Container) ArrayAppendOrCreateP(value interface{}, path string) error {
+	return container.ArrayAppendOrCreate(value, strings.Split(path, ".")...)
+}
+
+// LogAndSetError - Formats the message, log it and appends to the container's error path
+func (container *Container) LogAndSetError(format string, a ...interface{}) {
+	message := fmt.Sprintf(format, a...)
+	log.Println(message)
+	container.ArrayAppendOrCreateP(message, container.errorPath)
 }
